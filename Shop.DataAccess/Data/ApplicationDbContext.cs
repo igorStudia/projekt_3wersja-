80f955ff554c3aff0c539c1f925ea0354ebee80c
igorStudia <igorpuscikowskibiznes@gmail.com>
2024-02-12 21:44:23 +0100

    added project with several projects

---
+++ Shop.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Models;
+
+namespace Shop.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+            
+        }
+
+        //create table line
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1,Name="Sport",DisplayOrder=1},
+                new Category { Id = 2, Name = "Battle Royal", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+
+
+    }
+}
